b51dbb5960728a45b82803b82c3a6a996185f9df
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2019-03-23 14:43:54 -0700

    update set 1 chall 5

--- set1/chall5.py
+++ set1/chall5.py
@@ -2,14 +2,15 @@
 import sys
 from binascii import hexlify
 
-def xor(plain, key):
+def xor(a, key):
   ret = list()
-  for i, c in enumerate(plain):
-    ret.append(ord(c) ^ ord(key[i % len(key)]))
+  for i, c in enumerate(a):
+    ret.append(c ^ key[i % len(key)])
   return bytearray(ret)
 
 if __name__ == "__main__":
-  plain = open(sys.argv[1]).read()
-  key = sys.argv[2]
+  plain = bytearray([ord(c) for c in open(sys.argv[1]).read()])
+  key = bytearray([ord(c) for c in sys.argv[2]])
   print(hexlify(xor(plain, key)))
-  
+
+
